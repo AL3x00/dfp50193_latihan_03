8bbcda06061ad698b5835c4deab903afc418b851
AL3x00 <alexsaw2000@hotmail.com>
2021-10-04 12:04:04 +0800

    arahan-arahan asas

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 03</title>
+</head>
+
+<body>
+    <h1>Asas PHP</h1>
+
+    <?php
+    // komen satu baris
+    /*
+        komen banyak baris
+    */
+
+    $namapelajar = 'Saw';
+    echo "<h2>Hello $namapelajar</h2>";
+
+    $hadir = false;
+    $umur = 21;
+    $nokp = '000716020497';
+    ?>
+</body>
+
+</html>
